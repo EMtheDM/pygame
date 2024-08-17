ad78938963c1ce46734ab8eaa0e6489a99fa8679
Eric Martin <eric@Erics-MacBook-Pro.local>
2024-08-17 14:24:58 -0600

    added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our elements
+    # update everything
+    pygame.display.update()
+    clock.tick(60)
